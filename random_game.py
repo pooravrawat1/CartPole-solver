b14c831f464729d0ba11f1fe69764a9458954487
Poorav <pooravrawat@gmail.com>
2025-11-17 11:53:26 -0500

    Random game, works

--- random_game.py
+++ random_game.py
@@ -1,21 +1,15 @@
 import gymnasium as gym
 import random
 
-env = gym.make("CartPole-v1")
+env = gym.make("CartPole-v1", render_mode="human")
 
 def Random_games():
     # Each of this episode is its own game.
-    print("Starting random games...")
     for episode in range(10):
         print(f"Episode {episode + 1}")
         env.reset()
         # this is each frame, up to 500...but we wont make it that far with random.
         for t in range(500):
-            # This will display the environment
-            # Only display if you really want to see it.
-            # Takes much longer to display it.
-            env.render()
-            
             # This will just create a sample action in any environment.
             # In this environment, the action can be 0 or 1, which is left or right
             action = env.action_space.sample()
